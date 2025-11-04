99fbed521e9005179d892a41cbc54838dcf2dd19
sauceboss44 <alexrudakov5@gmail.com>
2025-11-04 23:07:49 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless'; // or '@astrojs/vercel/edge'
 
 export default defineConfig({
-  output: 'hybrid', // allows static + SSR/ISR
+  output: 'static' | 'server',
   site: 'https://uclquantumsociety.co.uk/',
 
   adapter: vercel({}), // serverless runtime on Vercel
